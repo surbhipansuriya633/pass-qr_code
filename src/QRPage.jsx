0d30deea1f7ad64219d4a93b1e0feaaf900c6556
surbhi <surbhipansuriya633@gmail.com>
2025-08-01 12:11:04 +0530

    set path in qrpage

--- src/QRPage.jsx
+++ src/QRPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { QRCodeCanvas } from "qrcode.react";
 
 function QRPage() {
-    const qrUrl = "https://pass-qr-code.vercel.app/";
+    const qrUrl = "https://pass-qr-code.vercel.app/season-pass-access";
 
     return (
         <div style={{ textAlign: "center", padding: 30 }}>
